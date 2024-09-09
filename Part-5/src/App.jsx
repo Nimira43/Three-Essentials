64051f0481c6880e209be3d2f6f9bb97193dc5b8
Nimira43 <n.rabone72@aol.com>
2024-09-09 04:33:43 +0100

    PointerLockControls demo

--- Part-5/src/App.jsx
+++ Part-5/src/App.jsx
@@ -1,22 +1,14 @@
 import { Canvas } from '@react-three/fiber'
-import Polyhedron from './Polyhedron'
-import * as THREE from 'three'
-import { Stats, OrbitControls } from '@react-three/drei'
+import { Stats, PointerLockControls } from '@react-three/drei'
 
 export default function App() {
-  const polyhedron = [
-    new THREE.BoxGeometry(),
-    new THREE.SphereGeometry(0.785398),
-    new THREE.DodecahedronGeometry(0.785398),
-  ]
-
   return (
-    <Canvas camera={{ position: [0, 0, 3] }}>
-      <Polyhedron position={[-0.75, -0.75, 0]} polyhedron={polyhedron} />
-      <Polyhedron position={[0.75, -0.75, 0]} polyhedron={polyhedron} />
-      <Polyhedron position={[-0.75, 0.75, 0]} polyhedron={polyhedron} />
-      <Polyhedron position={[0.75, 0.75, 0]} polyhedron={polyhedron} />
-      <OrbitControls />
+    <Canvas>
+      <mesh>
+        <boxGeometry args={[100, 10, 100, 100, 10, 100]} />
+        <meshBasicMaterial wireframe color={'lime'} />
+      </mesh>
+      <PointerLockControls />
       <Stats />
     </Canvas>
   )
