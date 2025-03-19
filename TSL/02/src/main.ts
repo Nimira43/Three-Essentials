6423b89c66ba97e03221757e88bb3ec3902db05e
Nimira43 <n.rabone72@aol.com>
2025-03-19 22:05:58 +0000

    Cleaned up code.

--- TSL/02/src/main.ts
+++ TSL/02/src/main.ts
@@ -3,9 +3,6 @@ import * as THREE from 'three/webgpu'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { abs, Fn, If, positionLocal, rotateUV, time, vec2 } from 'three/tsl'
 
-// import { color } from 'three/tsl'
-// import { texture, convertColorSpace } from 'three/tsl'
-
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -46,27 +43,10 @@ const main = Fn(() => {
 
 const material = new THREE.NodeMaterial()
 
-// Example 1 
-// material.fragmentNode = color('#ff4500')
-
-// Example 2
-// material.fragmentNode = convertColorSpace(
-//   texture(new THREE.TextureLoader().load('/billy.png')),
-//   THREE.SRGBColorSpace,
-//   THREE.LinearSRGBColorSpace
-// )
-
-// Example 3 and 5
-// material.fragmentNode = positionLocal
-
-// Example 4
 material.fragmentNode = main()
 
 const mesh = new THREE.Mesh(
   new THREE.PlaneGeometry(),
-  
-  // Example 5 
-  // new THREE.BoxGeometry(),
   material
 )
 scene.add(mesh)
